8bb4fb5649537733241e6851e133b4da85490101
annFromEarth <dv_anna@mail.ru>
2023-01-10 23:46:47 +0300

    reverse int commit1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let arr = n.toString().split('');
+  if (arr[0] == "0"|| arr[0] == "-") {
+    arr.shift();
+  }
+  let numberReversed=arr.reverse().join('');
+  return numberReversed;
 }
